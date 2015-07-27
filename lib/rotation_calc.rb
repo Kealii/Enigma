1686caba5bd7bab230065767402b01a4d9b00fdd
cminnerath <cminnerath@gmail.com>
2015-07-27 15:03:02 -0600

    rotation_calc start (pairing)
    nascent

---
+++ lib/rotation_calc.rb
@@ -0,0 +1,10 @@
+require_relative 'offsets'
+require_relative 'key_generator'
+
+class Rotation_calc
+
+  def a_rotation
+
+  end
+
+end
---
+++ test/rotation_calc_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/Rotation_calc'
+
+class Rotation_calcTest < Minitest::Test
+
+end
