64e60a98cb2f74fb26db8088672229ec583f0ab4
cminnerath <cminnerath@gmail.com>
2015-07-26 18:39:31 -0600

    Offsets work
    Tests passing for pulling date and squaring the date.

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -2,16 +2,16 @@ class Offsets
 
   attr_reader :date
 
-  def initialize(date = "010101")
+  def initialize(date = "111111")
     @date = date
   end
 
   def offset_date
-    date = Time.now.strftime("%d%m%y")
+    @date = Time.now.strftime("%d%m%y")
   end
 
-  # def square_date
-  #   datesquared = date.to_i ** 2
-  # end
+  def square_date
+    datesquared = date.to_i ** 2
+  end
 
 end
--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -6,7 +6,19 @@ class OffsetsTest < Minitest::Test
 
   def test_inits_date
     offset = Offsets.new
-    assert_equal offset.date, "010101"
+    assert_equal offset.date, "111111"
+  end
+
+  def test_pulls_date
+    offset = Offsets.new
+    date_test = offset.offset_date
+    assert_equal date_test, Time.now.strftime("%d%m%y")
+  end
+
+  def test_squares_date
+    offset = Offsets.new("111111")
+    square = offset.square_date
+    assert_equal square, 12345654321
   end
 
 end
