58b0fa44d1dca38f77e7e9b696e1fcf3bb6d82d5
cminnerath <cminnerath@gmail.com>
2015-07-27 10:50:29 -0600

    added files directory

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,11 +3,9 @@ require_relative 'encryptor'
 
 class Enigma
 
-input = File.open("#{ARGV[0]}", "r")
-output = File.open("#{ARGV[1]}", "w")
+  input = File.open("#{ARGV[0]}", "r")
+  output = File.open("#{ARGV[1]}", "w")
 
-puts "Created "
-
-
+  puts "Created #{output} using #{output}"
 
 end
