20ae3e3ef10b8b532d0eebce3d7a7731b364efee
cminnerath <cminnerath@gmail.com>
2015-07-27 11:46:53 -0600

    runner work
    input and output with tests, nascent

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,23 @@
 require_relative 'encryptor'
 
-
 class Enigma
 
-  input = File.open("#{ARGV[0]}", "r")
-  output = File.open("#{ARGV[1]}", "w")
+  attr_accessor :message, :final_message
 
-  puts "Created #{output} using #{output}"
+  def collect_message
+    @input = File.open("#{ARGV[0]}", "r")
+    @message = File.read(@input)
+  end
+
+  def output_message
+    @output = File.open("#{ARGV[1]}", "w")
+    @output.write(@message)
+    @output.close
+    @final_message = @message
+  end
+
+
+
+  puts "Created #{@output} using #{@input}"
 
 end
--- test/engima_test.rb
+++ test/engima_test.rb
@@ -1,7 +1,35 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/enigma'
+# require '../files/message.txt'
+# require '../files/encrypted.txt'
 
 class EnigmaTest < Minitest::Test
+  def test_we_can_read_input
+    e = Enigma.new
+    e.collect_message
+    e.output_message
+    assert_equal "test test test\n", e.message
+  end
+
+  def test_we_can_write_output
+    e = Enigma.new
+    e.collect_message
+    e.output_message
+    assert_equal "test test test\n", e.final_message
+  end
+
+
+  #
+  # def test_it_takes_input
+  #   mess = File.open("../files/message.txt", "r")
+  #   assert_equal "test test test", mess.read
+  # end
+
+  # def test_takes
+  #   e = Enigma.new
+  #   argvinput =
+  #
+  # end
 
 end
