6afc224ad2035850da984015a2ff7fd8fd5ec4e6
Michael Wong <michael.brian.wong@gmail.com>
2015-07-28 18:05:17 -0600

    added Character Map Tests

--- test/character_map_test.rb
+++ test/character_map_test.rb
@@ -3,5 +3,28 @@ require 'minitest/pride'
 require '../lib/character_map'
 
 class CharacterMapTest < Minitest::Test
+  def test_a_is_first_character_of_map
+    map = CharacterMap.new(0)
+    assert_equal "a", map.cipher['a']
+  end
 
+  def test_1_rotation_shifts_cipher
+    map = CharacterMap.new(1)
+    assert_equal 'b', map.cipher['a']
+  end
+
+  def test_0_is_26th_rotation
+    map = CharacterMap.new(26)
+    assert_equal 0, map.cipher['a']
+  end
+
+  def test_period_is_37th_rotation
+    map = CharacterMap.new(37)
+    assert_equal ".", map.cipher['a']
+  end
+
+  def test_39th_rotation_is_back_to_a
+    map = CharacterMap.new(39)
+    assert_equal 'a', map.cipher['a']
+  end
 end
